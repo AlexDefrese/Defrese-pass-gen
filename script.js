ec9a96a5828b1d1965fa13988263f900accaf70f
Alex Defrese <aidandefrese@gmail.com>
2022-08-13 15:40:45 -0400

    added prompts to get user input to arrays

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 // Assignment code here
+var characterLength = 8;
+var choiceArr = [];
+
 var numbers = ["1","2","3","4","5","6","7","8","9","0"];
 var specialChar = ["!","@","#","$","%","^","&","*","(",")"];
 var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
@@ -6,6 +9,11 @@ var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -16,5 +24,33 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+// should generate password based on user input
+}
+
+function userInput() {
+    characterLength = parseInt(window.prompt("How long would you like your password to be? Please choose between 8-128 characters."));
+
+    // ensure user answer is valid
+    if(isNaN(characterLength) || characterLength < 8 || characterLength > 128) {
+      window.alert("Please choose a character length betwee 8-128 characters. Thank you.");
+      return false;
+    }
+// adds lowercase letters to choice array
+    if (window.confirm("Would you like lower case letters in your password?")){ 
+        choiceArr = choiceArr.concat(lowerCase);
+    }
+    // adds uppercase letters to choice array
+    if (window.confirm("Would you like upper case letters in your password?")){ 
+        choiceArr = choiceArr.concat(upperCase);
+    }
+    // adds numbers to choice array
+    if (window.confirm("Would you like numbers in your password?")){ 
+      choiceArr = choiceArr.concat(numbers);
+  }
+  // adds special characters to choice array
+    if (window.confirm("Would you like special characters (!,@,#,$,%,^,&...) in your password?")){ 
+    choiceArr = choiceArr.concat(specialChar);
+}
+}
